d534c44b68bc167c198393f3ec902df4732d7436
Anton Zolotukhin <azolotukhin@lenovo.com>
2020-06-12 18:36:05 +0300

    init(core): initialize start structure of app (#develop) [0.0]

---
+++ src/app/modules/organizations/organizations.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { OrganizationsPageComponent } from './organizations-page/organizations-page.component';
+
+
+
+@NgModule({
+  declarations: [OrganizationsPageComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class OrganizationsModule { }
