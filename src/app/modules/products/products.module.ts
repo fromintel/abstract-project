4a40bda1c08b3bfda2b4682e6768d3d33e19280a
Anton Zolotukhin <azolotukhin@lenovo.com>
2020-06-13 08:26:51 +0300

    feat(core): example of service working (#product-service) [0.0]

--- src/app/modules/products/products.module.ts
+++ src/app/modules/products/products.module.ts
@@ -1,13 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProductsPageComponent } from './products-page/products-page.component';
+import { ProductsService } from '../../services/products/products.service';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
 
 @NgModule({
   declarations: [ProductsPageComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    ReactiveFormsModule,
+    FormsModule
+  ],
+  exports: [
+    ProductsPageComponent
+  ],
+  providers: [
+    ProductsService
   ]
 })
 export class ProductsModule { }
