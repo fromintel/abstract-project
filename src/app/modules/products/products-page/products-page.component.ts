86933fc031af7b0b0e0bf0c6aeb9ca9a11588e9a
Anton Zolotukhin <azolotukhin@lenovo.com>
2020-06-14 02:15:44 +0300

    feat(core): add action for the products (#actions) [0.0]

--- src/app/modules/products/products-page/products-page.component.ts
+++ src/app/modules/products/products-page/products-page.component.ts
@@ -3,7 +3,7 @@ import { ProductsService } from '../../../services/products/products.service';
 import { Subject } from 'rxjs';
 import { switchMap, takeUntil } from 'rxjs/operators';
 import { Product } from '../../../models/product';
-import { FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { AppStore } from '../../../store/app-store';
 
 @Component({
@@ -17,10 +17,22 @@ export class ProductsPageComponent implements OnInit, OnDestroy {
   public products: Product[] = [];
   public editProductForm: FormGroup;
   public addProductForm: FormGroup;
+  public selectedProduct: Product = null;
 
   constructor(
     private productsService: ProductsService
   ) {}
+
+  ngOnInit(): void {
+    this.initOrgsLoaded();
+    this.initEditForm();
+    this.initAddForm();
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
 
   private initOrgsLoaded(): void {
     AppStore.getOrgState()
@@ -37,14 +49,59 @@ export class ProductsPageComponent implements OnInit, OnDestroy {
       })
   }
 
-  ngOnInit(): void {
-    this.initOrgsLoaded();
+  private initEditForm(): void {
+    this.editProductForm = new FormGroup({
+      name: new FormControl('', Validators.required)
+    })
   }
 
-  ngOnDestroy(): void {
-    this.destroy$.next();
-    this.destroy$.complete();
+  private initAddForm(): void {
+    this.addProductForm = new FormGroup({
+      name: new FormControl('', Validators.required),
+      orgId: new FormControl('', Validators.required),
+    })
   }
 
+  private editProduct(product: Product): void {
+    const updatedProduct: Product = {
+      ...product,
+      name: this.editProductForm.get('name').value,
+      dateModified: new Date().toDateString()
+    };
+
+    this.productsService.editProductById(product.id, updatedProduct, this.products)
+      .pipe(takeUntil(this.destroy$))
+      .subscribe((data: Product[]) => {
+        this.products = data;
+        this.editProductForm.reset();
+      })
+  }
+
+  private addProduct(): void {
+    const newProduct: Product = {
+      name: this.addProductForm.get('name').value,
+      status: 'pending',
+      id: `f${(~~(Math.random()*1e8)).toString(16)}`,
+      orgId: this.addProductForm.get('orgId').value,
+      subId: this.products[0].subId,
+      dateCreated: new Date().toDateString(),
+      dateModified: new Date().toDateString(),
+    };
+
+    this.productsService.addProduct(newProduct, this.products)
+      .pipe(takeUntil(this.destroy$))
+      .subscribe((data: Product[]) => {
+        this.products = data;
+        this.addProductForm.reset();
+      });
+  }
+
+  public removeProduct(product: Product, currentProductsList: Product[]): void {
+    this.productsService.deleteProductById(product.id, currentProductsList)
+      .pipe(takeUntil(this.destroy$))
+      .subscribe((data: Product[]) => {
+        this.products = data;
+      })
+  }
 
 }
