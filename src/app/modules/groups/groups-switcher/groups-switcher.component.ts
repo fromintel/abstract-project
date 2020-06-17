8dab78658571688fb6a6f567519f196d6559a834
Konstantin Budzeev <budzeev@nixsolutions.com>
2020-06-17 15:42:56 +0300

    rename subscription to group

---
+++ src/app/modules/groups/groups-switcher/groups-switcher.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-groups-switcher',
+  templateUrl: './groups-switcher.component.html',
+  styleUrls: ['./groups-switcher.component.scss']
+})
+export class GroupsSwitcherComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
