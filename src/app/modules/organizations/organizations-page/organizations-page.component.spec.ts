d90932f85e870d8d6bc11fa6baf2dc2ede3c1924
Anton <antn.vryckij@gmail.com>
2020-06-21 14:05:51 +0300

    add mock inn test

--- src/app/modules/organizations/organizations-page/organizations-page.component.spec.ts
+++ src/app/modules/organizations/organizations-page/organizations-page.component.spec.ts
@@ -1,8 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { MockComponent } from 'ng-mocks';
 import { OrganizationsPageComponent } from './organizations-page.component';
-import {PreloaderComponent} from '../preloader/preloader.component';
-import {OrganizationsService} from '../../../services/organizations/organizations.service';
+import { PreloaderComponent } from '../preloader/preloader.component';
+import { OrganizationsService } from '../../../services/organizations/organizations.service';
 
 describe('OrganizationsPageComponent', () => {
   let component: OrganizationsPageComponent;
@@ -10,7 +10,9 @@ describe('OrganizationsPageComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [ OrganizationsPageComponent, PreloaderComponent ],
+      declarations: [
+        OrganizationsPageComponent,
+        MockComponent(PreloaderComponent) ],
       providers: [ OrganizationsService ]
     });
 
