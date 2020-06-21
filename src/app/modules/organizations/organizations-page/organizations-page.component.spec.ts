26d3760a2776207ca6c14bdd7c58dd13a9697e5a
Anton <antn.vryckij@gmail.com>
2020-06-21 14:29:42 +0300

    add stub service in test file

--- src/app/modules/organizations/organizations-page/organizations-page.component.spec.ts
+++ src/app/modules/organizations/organizations-page/organizations-page.component.spec.ts
@@ -2,7 +2,8 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { MockComponent } from 'ng-mocks';
 import { OrganizationsPageComponent } from './organizations-page.component';
 import { PreloaderComponent } from '../preloader/preloader.component';
-import { OrganizationsService } from '../../../services/organizations/organizations.service';
+import { OrganizationsServiceStub } from '../../../services/stubs/organization.service.stub';
+import {OrganizationsService} from '../../../services/organizations/organizations.service';
 
 describe('OrganizationsPageComponent', () => {
   let component: OrganizationsPageComponent;
@@ -13,7 +14,10 @@ describe('OrganizationsPageComponent', () => {
       declarations: [
         OrganizationsPageComponent,
         MockComponent(PreloaderComponent) ],
-      providers: [ OrganizationsService ]
+      providers: [ {
+        provide: OrganizationsService,
+        useClass: OrganizationsServiceStub
+      } ]
     });
 
     fixture = TestBed.createComponent(OrganizationsPageComponent);
