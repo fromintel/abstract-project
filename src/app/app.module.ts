b4a69ff35dfcdf6ee2848023668fb7d35f7c4648
Nikita <nikitos5153@gmail.com>
2020-06-29 15:07:32 +0300

    added opening of modal for adding product

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,18 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { ProductsModule } from './modules/products/products.module';
-import { OrganizationsModule } from './modules/organizations/organizations.module';
-import { GroupsService } from './services/groups/groups.service';
-import { FormsModule } from '@angular/forms';
-import { GroupsSwitcherComponent } from './modules/groups/groups-switcher/groups-switcher.component';
-import { SidebarComponent } from './components/sidebar/sidebar.component';
-import { NavigationComponent } from './components/navigation/navigation.component';
-import { AddProductFormComponent } from './components/add-product-form/add-product-form.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { ProductsModule } from "./modules/products/products.module";
+import { OrganizationsModule } from "./modules/organizations/organizations.module";
+import { GroupsService } from "./services/groups/groups.service";
+import { FormsModule } from "@angular/forms";
+import { GroupsSwitcherComponent } from "./modules/groups/groups-switcher/groups-switcher.component";
+import { SidebarComponent } from "./components/sidebar/sidebar.component";
+import { NavigationComponent } from "./components/navigation/navigation.component";
+import { AddProductFormComponent } from "./components/add-product-form/add-product-form.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { ModalModule } from "ngx-bootstrap/modal";
 
 @NgModule({
   declarations: [
@@ -20,14 +22,17 @@ import { AddProductFormComponent } from './components/add-product-form/add-produ
     NavigationComponent,
     AddProductFormComponent
   ],
+  entryComponents: [AddProductFormComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     ProductsModule,
     OrganizationsModule,
-    FormsModule
+    FormsModule,
+    BrowserAnimationsModule,
+    ModalModule.forRoot()
   ],
   providers: [GroupsService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/modules/products/products-page/products-page.component.ts
+++ src/app/modules/products/products-page/products-page.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { BsModalService, BsModalRef } from "ngx-bootstrap/modal";
+import { AddProductFormComponent } from "src/app/components/add-product-form/add-product-form.component";
 
 @Component({
-  selector: 'app-products-page',
-  templateUrl: './products-page.component.html',
-  styleUrls: ['./products-page.component.scss']
+  selector: "app-products-page",
+  templateUrl: "./products-page.component.html",
+  styleUrls: ["./products-page.component.scss"]
 })
 export class ProductsPageComponent implements OnInit {
+  public bsModalRef: BsModalRef;
 
-  constructor() {}
+  constructor(private modalService: BsModalService) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  handleOpenAddProductForm(): void {
+    this.bsModalRef = this.modalService.show(AddProductFormComponent);
   }
-
 }
